ed5a33337fc18101c5372bf4f16bbc0ad75589bb
Mågnëum / <krakinzkon@gmail.com>
2023-03-14 21:42:04 +0530

    updated

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Search from "./dist/web/index.js";
-import { AudioMeta, AudioSave } from "./dist/audio/index.js";
-import { VideoMeta, VideoSave, VideoAudioSave } from "./dist/video/index.js";
+import Search from "./lib/web/index.js";
+import { AudioMeta, AudioSave } from "./lib/audio/index.js";
+import { VideoMeta, VideoSave, VideoAudioSave } from "./lib/video/index.js";
 export { VideoMeta, VideoSave, VideoAudioSave, AudioMeta, AudioSave, Search };
