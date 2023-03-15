f93c1c632f7076fab703393e8ac1b719f684bb6a
Mågnëum / <krakinzkon@gmail.com>
2023-03-16 03:11:00 +0530

    updated

--- test/index.js
+++ test/index.js
@@ -1,93 +1,46 @@
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import * as dotenv from "dotenv";
-import ExecJson from "youtube-dl-exec";
 const url = "https://youtu.be/0XyV-vw5II4";
 const four = "https://youtu.be/TpdapO9QGRo";
-import ProgressEstimator from "progress-estimator";
-const ProgressEst = ProgressEstimator();
-dotenv.config();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { videoadvanced } from "../lib/utils/video.js";
-(async () => {
-  const jsonmeta = await ProgressEst(
-    ExecJson(four, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const data = await videoadvanced(jsonmeta, "2160p");
-  console.log(data);
-  videoadvanced(jsonmeta, "2160p").then((data) => console.log(data));
-})();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { videocustomquality } from "../lib/utils/video.js";
-(async () => {
-  const jsonmeta = await ProgressEst(
-    ExecJson(four, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const data = await videocustomquality(jsonmeta, "highest");
-  console.log(data);
-  videocustomquality(jsonmeta, "highest").then((data) => console.log(data));
-})();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+
 import { audioseperator } from "../lib/utils/audio.js";
 (async () => {
-  const jsonmeta = await ProgressEst(
-    ExecJson(four, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const data = await audioseperator(jsonmeta, "medium");
+  audioseperator(four, "medium").then((data) => console.log(data));
+  const data = await audioseperator(four, "medium");
   console.log(data);
-  audioseperator(jsonmeta, "medium").then((data) => console.log(data));
 })();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+
 import { audiocustomquality } from "../lib/utils/audio.js";
 (async () => {
-  const jsonmeta = await ProgressEst(
-    ExecJson(four, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const data = await audiocustomquality(jsonmeta, "best-possible");
+  audiocustomquality(four, "best-possible").then((data) => console.log(data));
+  const data = await audiocustomquality(four, "best-possible");
   console.log(data);
-  audiocustomquality(jsonmeta, "best-possible").then((data) =>
-    console.log(data)
-  );
 })();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { audiosave } from "../lib/audio/index.js";
+
+import { videoadvanced } from "../lib/utils/video.js";
 (async () => {
-  const data = await audiosave(four, "medium", "./src/");
+  videoadvanced(four, "2160p").then((data) => console.log(data));
+  const data = await videoadvanced(four, "2160p");
   console.log(data);
-  audiosave(four, "medium", "./src/").then((data) => console.log(data));
 })();
-("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { audiometa } from "../lib/audio/index.js";
+
+import { videocustomquality } from "../lib/utils/video.js";
 (async () => {
-  const data = await audiometa(four);
+  videocustomquality(four, "highest").then((data) => console.log(data));
+  const data = await videocustomquality(four, "highest");
   console.log(data);
-  audiometa(four).then((data) => console.log(data));
 })();
+
+// import { audiosave } from "../lib/audio/index.js";
+// (async () => {
+// const data = await audiosave(four, "medium", "./src/");
+// console.log(data);
+// audiosave(four, "medium", "./src/").then((data) => console.log(data));
+// })();
+
+// import { audiometa } from "../lib/audio/index.js";
+// (async () => {
+// const data = await audiometa(four);
+// console.log(data);
+// audiometa(four).then((data) => console.log(data));
+// })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
