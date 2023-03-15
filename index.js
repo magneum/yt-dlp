0ae0804bdb4bafc14696ec3502c3a2754b03c4c8
Mågnëum / <krakinzkon@gmail.com>
2023-03-16 04:27:21 +0530

    ytdlp - python + javascript youtube client
    ytdlp - python + javascript youtube client

---
+++ index.js
@@ -0,0 +1,4 @@
+import Search from "./lib/web/index.js";
+import { audiometa, audiosave } from "./lib/audio/index.js";
+import { videometa, videosave, videoaudiosave } from "./lib/video/index.js";
+export { videometa, videosave, videoaudiosave, audiometa, audiosave, Search };
