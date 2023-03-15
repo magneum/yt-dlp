e2b1c22ad395ddea24650962ca61e942b7a4f6c1
Mågnëum / <krakinzkon@gmail.com>
2023-03-16 02:34:25 +0530

    updated

--- test/index.js
+++ test/index.js
@@ -9,39 +9,51 @@ import ProgressEstimator from "progress-estimator";
 const ProgressEst = ProgressEstimator();
 dotenv.config();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { VideoAdvanced, VideoCustom } from "../utils/video.js";
-(async () => {
-  const cron = await ProgressEst(
-    ExecJson(url, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const video_ = await VideoCustom(cron, "highest");
-  console.log("CUSTOM VIDEO: ", video_);
-  const _video = await VideoAdvanced(cron, "2160p");
-  console.log("ADVANCE VIDEO: ", _video);
-})();
+// import { VideoAdvanced, VideoCustom } from "../utils/video.js";
+// (async () => {
+// const cron = await ProgressEst(
+// ExecJson(url, {
+// noWarnings: true,
+// dumpSingleJson: true,
+// preferFreeFormats: true,
+// noCheckCertificates: true,
+// addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+// }),
+// "Obtaining: " + " "
+// );
+// const video_ = await VideoCustom(cron, "highest");
+// const _video = await VideoAdvanced(cron, "2160p");
+// console.log("CUSTOM VIDEO: ", video_);
+// console.log("ADVANCE VIDEO: ", _video);
+// })();
+// ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// import { AudioSeperator, AudioQualityCustom } from "../utils/audio.js";
+// (async () => {
+// const cron = await ProgressEst(
+// ExecJson(url, {
+// noWarnings: true,
+// dumpSingleJson: true,
+// preferFreeFormats: true,
+// noCheckCertificates: true,
+// addHeader: ["referer:youtube.com", "user-agent:googlebot"],
+// }),
+// "Obtaining: " + " "
+// );
+// const audio_ = await AudioSeperator(cron, "medium");
+// const _audio = await AudioQualityCustom(cron, "best-possible");
+// console.log("AUDIO: ", audio_);
+// console.log("CUSTOM AUDIO: ", _audio);
+// })();
+// ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+// import { AudioSave } from "../lib/audio/index.js";
+// (async () => {
+// const datalog = await AudioSave(url, "medium", "./src/");
+// console.log(datalog);
+// })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
-import { AudioSeperator, AudioQualityCustom } from "../utils/audio.js";
+import { AudioMeta } from "../lib/audio/index.js";
 (async () => {
-  const cron = await ProgressEst(
-    ExecJson(url, {
-      noWarnings: true,
-      dumpSingleJson: true,
-      preferFreeFormats: true,
-      noCheckCertificates: true,
-      addHeader: ["referer:youtube.com", "user-agent:googlebot"],
-    }),
-    "Obtaining: " + " "
-  );
-  const audio_ = await AudioSeperator(cron, "medium");
-  console.log("AUDIO: ", audio_);
-  const _audio = await AudioQualityCustom(cron, "best-possible");
-  console.log("CUSTOM AUDIO: ", _audio);
+  const datalog = await AudioMeta(url);
+  console.log(datalog);
 })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
--- utils/audio.js
+++ utils/audio.js
@@ -56,7 +56,7 @@ export async function AudioSeperator(json) {
   }
 }
 
-export async function AudioQualityCustom(json, quality) {
+export async function AudioQualityCustom(json) {
   try {
     const medium = json.formats.filter(
       (res) =>
@@ -137,3 +137,96 @@ export async function AudioQualityCustom(json, quality) {
   }
 }
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
+export async function AudioMetaData(json, quality) {
+  try {
+    const medium = json.formats.filter(
+      (res) =>
+        (res.format_id === "140" &&
+          res.format_note === "medium" &&
+          res.resolution === "audio only") ||
+        (res.format_id === "251" &&
+          res.format_note === "medium" &&
+          res.resolution === "audio only")
+    );
+    const low = json.formats.filter(
+      (res) =>
+        (res.format_id === "139" &&
+          res.format_note === "low" &&
+          res.resolution === "audio only") ||
+        (res.format_id === "249" &&
+          res.format_note === "low" &&
+          res.resolution === "audio only") ||
+        (res.format_id === "250" &&
+          res.format_note === "low" &&
+          res.resolution === "audio only")
+    );
+    const ultralow = json.formats.filter(
+      (res) =>
+        (res.format_id === "599" &&
+          res.format_note === "ultralow" &&
+          res.resolution === "audio only") ||
+        (res.format_id === "600" &&
+          res.format_note === "ultralow" &&
+          res.resolution === "audio only")
+    );
+
+    return [
+      {
+        medium: {
+          filesize:
+            medium[0].filesize || medium[1].filesize || medium.filesize || null,
+          audiochannels:
+            medium[0].audio_channels ||
+            medium[1].audio_channels ||
+            medium.audio_channels ||
+            null,
+          extensions: medium[0].ext || medium[1].ext || medium.ext || null,
+          acodec: medium[0].acodec || medium[1].acodec || medium.acodec || null,
+          url: medium[0].url || medium[1].url || medium.url || null,
+        },
+
+        low: {
+          filesize:
+            low[0].filesize ||
+            low[1].filesize ||
+            low[2].filesize ||
+            low.filesize ||
+            null,
+          audiochannels:
+            low[0].audio_channels ||
+            low[1].audio_channels ||
+            low[2].audio_channels ||
+            low.audio_channels ||
+            null,
+          extensions: low[0].ext || low[1].ext || low[2].ext || low.ext || null,
+          acodec:
+            low[0].acodec ||
+            low[1].acodec ||
+            low[2].acodec ||
+            low.acodec ||
+            null,
+          url: low[0].url || low[1].url || low[2].url || low.url || null,
+        },
+        ultralow: {
+          filesize:
+            ultralow[0].filesize ||
+            ultralow[1].filesize ||
+            ultralow.filesize ||
+            null,
+          audiochannels:
+            ultralow[0].audio_channels ||
+            ultralow[1].audio_channels ||
+            ultralow.audio_channels ||
+            null,
+          extensions:
+            ultralow[0].ext || ultralow[1].ext || ultralow.ext || null,
+          acodec:
+            ultralow[0].acodec || ultralow[1].acodec || ultralow.acodec || null,
+          url: ultralow[0].url || ultralow[1].url || ultralow.url || null,
+        },
+      },
+    ];
+  } catch (error) {
+    return error.message;
+  }
+}
