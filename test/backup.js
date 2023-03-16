45f2e03c21802a6f59059afe385fe0a2b5f9ee6f
Mågnëum / <krakinzkon@gmail.com>
2023-03-16 11:46:50 +0530

    yt-dlp - Python+ Javascript Youtube Audio Video Scrapper And Downloader Client

--- test/backup.js
+++ test/backup.js
@@ -1,20 +1,20 @@
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 import { infoAudio_customQuality } from "../index.js";
+url = "https://youtu.be/TpdapO9QGRo";
+sort = "medium";
+
 /**
  * @param {url} url -> "youtube-video-link"
  * @param {string} sort -> "medium" || "low" || "ultralow"
  */
 
+// Promise method
+infoAudio_customQuality(url, sort)
+  .then((data) => console.log("INFOAUDIO_CUSTOMQUALITY: ", data))
+  .catch((e) => console.log(e));
+
+// async/await method
 (async () => {
-  let sort = "medium";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  infoAudio_customQuality(url, sort)
-    .then((data) => console.log("INFOAUDIO_CUSTOMQUALITY: ", data))
-    .catch((e) => console.log(e));
-
-  // async/await method
   const data = await infoAudio_customQuality(url, sort).catch((e) =>
     console.log(e)
   );
@@ -22,20 +22,21 @@ import { infoAudio_customQuality } from "../index.js";
 })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 import { infoAudio_Sorted } from "../index.js";
+url = "https://youtu.be/TpdapO9QGRo";
+quality = "best-possible";
+
 /**
  * @param {url} url -> "youtube-video-link"
  * @param {string} quality -> "best-possible" || "medium-possible" || "lowest-possible"
  */
+
+// Promise method
+infoAudio_Sorted(url, quality)
+  .then((data) => console.log("INFOAUDIO_SORTED: ", data))
+  .catch((e) => console.log(e));
+
+// async/await method
 (async () => {
-  let quality = "best-possible";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  infoAudio_Sorted(url, quality)
-    .then((data) => console.log("INFOAUDIO_SORTED: ", data))
-    .catch((e) => console.log(e));
-
-  // async/await method
   const data = await infoAudio_Sorted(url, quality).catch((e) =>
     console.log(e)
   );
@@ -43,20 +44,21 @@ import { infoAudio_Sorted } from "../index.js";
 })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 import { infoVideo_customResolution } from "../index.js";
+url = "https://youtu.be/TpdapO9QGRo";
+resolution = "2160p";
+
 /**
  * @param {url} url -> "youtube-video-link"
  * @param {string} resolution -> "2160p" || "1440p" || "1080p" || "720p" || "480p" || "360p" || "240p" || "144p"
  */
+
+// Promise method
+infoVideo_customResolution(url, resolution)
+  .then((data) => console.log("INFOVIDEO_CUSTOMRESOLUTION: ", data))
+  .catch((e) => console.log(e));
+
+// async/await method
 (async () => {
-  let resolution = "2160p";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  infoVideo_customResolution(url, resolution)
-    .then((data) => console.log("INFOVIDEO_CUSTOMRESOLUTION: ", data))
-    .catch((e) => console.log(e));
-
-  // async/await method
   const data = await infoVideo_customResolution(url, resolution).catch((e) =>
     console.log(e)
   );
@@ -64,20 +66,20 @@ import { infoVideo_customResolution } from "../index.js";
 })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 import { infoVideo_Sorted } from "../index.js";
+url = "https://youtu.be/TpdapO9QGRo";
+quality = "highest";
+
 /**
  * @param {url} url -> "youtube-video-link"
  * @param {string} resolution -> "highest" || "medium" || "lowest"
  */
+// Promise method
+infoVideo_Sorted(url, quality)
+  .then((data) => console.log("INFOVIDEO_SORTED: ", data))
+  .catch((e) => console.log(e));
+
+// async/await method
 (async () => {
-  let quality = "highest";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  infoVideo_Sorted(url, quality)
-    .then((data) => console.log("INFOVIDEO_SORTED: ", data))
-    .catch((e) => console.log(e));
-
-  // async/await method
   const data = await infoVideo_Sorted(url, quality).catch((e) =>
     console.log(e)
   );
@@ -85,6 +87,8 @@ import { infoVideo_Sorted } from "../index.js";
 })();
 // ☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱ [ DOWNLOADER ] ☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱
 import { downloadAudio_customQuality } from "../lib/audio/ffmpeg.js";
+url = "https://youtu.be/TpdapO9QGRo";
+sort = "medium";
 
 /**
  * @param {url} url -> "youtube-video-link"
@@ -92,18 +96,15 @@ import { downloadAudio_customQuality } from "../lib/audio/ffmpeg.js";
  * @param {download-folder-name} @arg {optional} -> auto created if not exists or specified
  */
 
+// Promise method
+downloadAudio_customQuality(url, sort, "mågneum")
+  .then((data) =>
+    console.log(chalk.bgGreen("DOWNLOADAUDIO_CUSTOMQUALITY:"), data)
+  )
+  .catch((e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message)));
+
 // async/await method
 (async () => {
-  let sort = "medium";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  downloadAudio_customQuality(url, sort, "mågneum")
-    .then((data) =>
-      console.log(chalk.bgGreen("DOWNLOADAUDIO_CUSTOMQUALITY:"), data)
-    )
-    .catch((e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message)));
-
   const data = await downloadAudio_customQuality(url, sort, "./src/").catch(
     (e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message))
   );
@@ -111,6 +112,8 @@ import { downloadAudio_customQuality } from "../lib/audio/ffmpeg.js";
 })();
 ("◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎");
 import { downloadAudio_Sorted } from "../lib/audio/ffmpeg.js";
+url = "https://youtu.be/TpdapO9QGRo";
+sort = "medium";
 
 /**
  * @param {url} url -> "youtube-video-link"
@@ -118,16 +121,13 @@ import { downloadAudio_Sorted } from "../lib/audio/ffmpeg.js";
  * @param {download-folder-name} @arg {optional} -> auto created if not exists or specified
  */
 
+// Promise method
+downloadAudio_Sorted(url, "best-possible", "mågneum")
+  .then((data) => console.log(chalk.bgGreen("DOWNLOADAUDIO_SORTED:"), data))
+  .catch((e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message)));
+
 // async/await method
 (async () => {
-  let sort = "medium";
-  const url = "https://youtu.be/TpdapO9QGRo";
-
-  // Promise method
-  downloadAudio_Sorted(url, "best-possible", "mågneum")
-    .then((data) => console.log(chalk.bgGreen("DOWNLOADAUDIO_SORTED:"), data))
-    .catch((e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message)));
-
   const data = await downloadAudio_Sorted(url, "best-possible", "./src/").catch(
     (e) => console.log(chalk.bgRed("ERROR: "), chalk.gray(e.message))
   );
