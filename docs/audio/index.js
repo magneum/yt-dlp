4dae6bb6783ac211d16d0efc712f8fbf9971008c
Mågnëum / <krakinzkon@gmail.com>
2023-03-18 10:02:42 +0530

    yt-dlp: youtube download client

--- docs/audio/index.js
+++
@@ -1,4 +0,0 @@
-// ◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎"
-import audiometa from "./audiometa.js";
-import audiosave from "./audiosave.js";
-export { audiometa, audiosave };
--- docs/video/index.js
+++
@@ -1,5 +0,0 @@
-// ◎☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱[ ву mågneum ]☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱☱◎"
-import videometa from "./videometa.js";
-import videosave from "./videosave.js";
-import videoaudiosave from "./videoaudiosave.js";
-export { videometa, videosave, videoaudiosave };
